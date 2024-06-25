e0b0a5fdc4329ff112dacfae33e7d4f5ef111764
KnBalaurx <bm713648@gmail.com>
2024-06-25 11:50:59 -0400

    implementacion de varias pagina linkeadas al index
    en una se agrego un formulario y en otra una calculadora

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
-import Perfil from "./componentes/Perfil";
+import Link from "next/link";
+import Perfil from "./Componentes/Perfil";
+
+
 
 export default function Home() {
   return (
     <>
-     <p>Hola</p>
-     <p>Chao</p>
-      <Perfil nombre="Pablo" apellido="Marmol" edad={30} rol="docente"/>
-      <Perfil nombre="Jose" apellido="Perez" edad={15}/>
+      <nav>
+        <Link href={"./Componentes/Pagina1"}>Pagina1</Link>
+        <Link href={"./Componentes/Pagina2"}>Calculadora</Link>
+      </nav>
+
+      <p>Hola</p>
+      <p>Chao</p>
+        <Perfil nombre="Pablo" apellido="Marmol" edad={30} rol="docente"/>
+        <Perfil nombre="Jose" apellido="Perez" edad={15}/>
     </>
   );
 }
